b2b1b9b8ac42e9f251cece1ec652a3a1b960c74f
Zeosev <zeosev@gmail.com>
2016-01-07 18:06:25 +0200

    Create work_with_strings.java

---
+++ work_with_strings.java
@@ -0,0 +1,20 @@
+public static void main(String[] args) {
+		
+		// 1. Друкую довільні два рядки "a" i "b"
+		String a = " Hello from Java ";
+		System.out.println (a);
+		
+		// 2. Друкую рядок, з використанням складання рядків "a" + "b"
+		String b = " Development ";
+		System.out.println(a + b);
+		
+		// 3. Об'єдную рядок з числом
+		int c = 7;
+		System.out.println(a + c + b);
+		
+		// 4. Використовую System.out.prin(...) для виводу на екран
+		System.out.print(a + c + b);
+		
+		// 5. Дізнаюсь чи можливо вивести на екран порожній рядок :)
+		String text = "  ";
+		System.out.print (text);
