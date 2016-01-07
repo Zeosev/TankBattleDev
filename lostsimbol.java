63f363bdef751918b52b5437a39b3f0e9edc4e62
Zeosev <zeosev@gmail.com>
2016-01-07 20:52:58 +0200

    Create lostsimbol.java

---
+++ lostsimbol.java
@@ -0,0 +1,6 @@
+public class lostsymbol {
+	public static void main(String[] args) {
+	        String mjQuote = "I'm filed over and over and over again in my life and that is why I succeed!";
+			System.out.print(mjQuote.charAt(mjQuote.length()-1));
+	}
+}
